1efa867d16d68b0a2a8d673fd219b6aa860533d7
isLiuHao <603400054@qq.com>
2021-02-26 15:35:46 +0800

    LeetCode练习

--- src/leetcode/demo040.java
+++ src/leetcode/demo040.java
@@ -6,20 +6,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class demo039 {
+public class demo040 {
 
     List<List<Integer>> lists=new ArrayList<>();
     List<Integer> list=new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         dfs(candidates,target,0);
         return lists;
     }
     public void dfs(int[] candidates,int target,int index){//index表示使用candidates第几个
         for(int i=index;i<candidates.length;i++){
+            if(i>index&&candidates[i]==candidates[i-1])continue;
             if(candidates[i]<target){
                 list.add(candidates[i]);
-                dfs(candidates,target-candidates[i],i);
+                dfs(candidates,target-candidates[i],i+1);
                 list.remove(list.size()-1);
             }
             else if(candidates[i]==target){  //最后一个
@@ -34,8 +35,8 @@ public class demo039 {
     }
     @Test
     public void test(){
-        int[] candidates=new int[]{2,3,6,7};
-        combinationSum(candidates,7);
+        int[] candidates=new int[]{1,1,2,5,6,7,10};
+        combinationSum2(candidates,8);
         System.out.println(lists);
     }
 }
