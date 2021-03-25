9acd2a5b4544f5586e98ec64c1f230ef15c1854a
isLiuHao <603400054@qq.com>
2021-03-25 23:58:09 +0800

    LeetCode

--- src/leetcode/demo207.java
+++ src/leetcode/demo207.java
@@ -1,10 +1,12 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class demo207 {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    public boolean canFinish1(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adjacency = new ArrayList<>();
         for(int i = 0; i < numCourses; i++)
             adjacency.add(new ArrayList<>());
@@ -28,6 +30,36 @@ public class demo207 {
             if(!dfs(adjacency,j,flags))return false;
         }
         flags[i] = -1;//遍历完改为 其他点访问
+        return true;
+    }
+
+    //拓扑排序
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> list=new ArrayList<>();//邻接表存储
+        int[] indegree=new int[numCourses];
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int[] cp:prerequisites){
+            list.get(cp[1]).add(cp[0]);
+            indegree[cp[1]]++;
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0)
+                queue.add(i);
+        }
+        while(!queue.isEmpty()){
+            int i=queue.poll();
+            for(int x:list.get(i)){
+                indegree[x]--;
+                if(indegree[x]==0)
+                    queue.add(x);
+            }
+        }
+        for(int i=0;i<numCourses;i++) {
+            if (indegree[i] != 0)return false;
+        }
         return true;
     }
 }
