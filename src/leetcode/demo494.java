c29b47af66d64e5c15833f76f3ed83db52e09286
isLiuHao <603400054@qq.com>
2021-04-07 00:26:49 +0800

    LeetCode

---
+++ src/leetcode/demo494.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class demo494 {
+    int count=0;
+    public int findTargetSumWays(int[] nums, int S) {
+        calculate(nums,0,0, S);
+        return count;
+    }
+    public void calculate(int[] nums, int i, int sum, int S) {
+        if(i==nums.length){
+            if(sum==S)count++;
+        }else{
+            calculate(nums,i+1,sum+nums[i],S);
+            calculate(nums,i+1,sum-nums[i],S);
+        }
+    }
+    @Test
+    public void test(){
+        int[] a={1, 1, 1, 1, 1};
+        findTargetSumWays(a,3);
+    }
+}
